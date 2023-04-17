294ccedfb34bfa38935327df369842e17fd5ac7d
Krastan Draganov <krastan.draganov@gmail.com>
2023-04-17 12:18:43 +0300

    Working on Sign Up Flow Configuration

--- frontend/src/sign-up/SignUpFlow.tsx
+++ frontend/src/sign-up/SignUpFlow.tsx
@@ -1,5 +1,14 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
-import { StyleProp, Text, View, ViewStyle } from "react-native";
+import {
+  Animated,
+  Image,
+  Pressable,
+  StyleProp,
+  Text,
+  TextStyle,
+  View,
+  ViewStyle,
+} from "react-native";
 import { CustomButton } from "../components/CustomButton";
 import { AppConfiguration } from "../config";
 import { ColorConstants } from "../constants/colors";
@@ -8,11 +17,22 @@ import { CountriesDropdown } from "./CountriesDropdown";
 import { CustomTextInput } from "./CustomTextInput";
 import { SignUpFlowConfiguration } from "./SignUpFlowType";
 
+const whiteContainerWidth = 390;
+
+const titleFontSize = 32;
+const titleFontWeight: "400" = "400";
+const titleMarginBottom = 30;
+
 const marginAboveTextInput = 10;
 
-const buttonHeight = 60;
-const buttonLabelFontSize = 19;
-const buttonLabelWeight = "400";
+const buttonNextHeight = 60;
+const buttonNextLabelFontSize = 19;
+const buttonNextLabelWeight: "400" = "400";
+
+const buttonPreviousSize = 32;
+
+const arrowIconImageHeight = 22;
+const arrowIconImageWidth = 12;
 
 function SignUpFlowStepCustomerDetails(props: { onNextPress: () => void }) {
   const [countries, setCountries] = useState([]);
@@ -38,8 +58,51 @@ function SignUpFlowStepCustomerDetails(props: { onNextPress: () => void }) {
   const [givenNames, setGivenNames] = useState("");
   const [lastName, setLastName] = useState("");
 
+  const titleContainerStyle = useMemo(
+    (): StyleProp<ViewStyle> => ({
+      alignItems: "center",
+      justifyContent: "center",
+    }),
+    []
+  );
+
+  const titleTextStyle = useMemo(
+    (): StyleProp<TextStyle> => ({
+      fontSize: titleFontSize,
+      fontWeight: titleFontWeight,
+      color: ColorConstants.BLACK,
+      marginBottom: titleMarginBottom,
+    }),
+    []
+  );
+
+  const buttonNextLabelStyle = useMemo(
+    (): StyleProp<TextStyle> => ({
+      fontSize: buttonNextLabelFontSize,
+      fontWeight: buttonNextLabelWeight,
+      color: ColorConstants.WHITE,
+    }),
+    []
+  );
+
+  const buttonNextContainerStyle = useMemo(
+    () => ({
+      height: buttonNextHeight,
+      borderRadius: StyleConstants.BORDER_RADIUS,
+      backgroundColor: ColorConstants.BLUE,
+      marginLeft: StyleConstants.MARGIN,
+      marginRight: StyleConstants.MARGIN,
+      marginBottom: StyleConstants.MARGIN,
+    }),
+    []
+  );
+
   return (
     <View>
+      <View style={titleContainerStyle}>
+        <Text style={titleTextStyle}>Sign Up</Text>
+      </View>
+
       <CountriesDropdown countries={countries} />
 
       <CustomTextInput
@@ -73,19 +136,8 @@ function SignUpFlowStepCustomerDetails(props: { onNextPress: () => void }) {
       <CustomButton
         onPress={onPress}
         labelText="Next"
-        labelStyle={{
-          fontSize: buttonLabelFontSize,
-          fontWeight: buttonLabelWeight,
-          color: ColorConstants.WHITE,
-        }}
-        containerStyle={{
-          height: buttonHeight,
-          borderRadius: StyleConstants.BORDER_RADIUS,
-          backgroundColor: ColorConstants.BLUE,
-          marginLeft: StyleConstants.MARGIN,
-          marginRight: StyleConstants.MARGIN,
-          marginBottom: StyleConstants.MARGIN,
-        }}
+        labelStyle={buttonNextLabelStyle}
+        containerStyle={buttonNextContainerStyle}
       />
     </View>
   );
@@ -99,8 +151,51 @@ function SignUpFlowStepLoginDetails(props: { onNextPress: () => void }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const titleContainerStyle = useMemo(
+    (): StyleProp<ViewStyle> => ({
+      alignItems: "center",
+      justifyContent: "center",
+    }),
+    []
+  );
+
+  const titleTextStyle = useMemo(
+    (): StyleProp<TextStyle> => ({
+      fontSize: titleFontSize,
+      fontWeight: titleFontWeight,
+      color: ColorConstants.BLACK,
+      marginBottom: StyleConstants.MARGIN,
+    }),
+    []
+  );
+
+  const buttonNextLabelStyle = useMemo(
+    (): StyleProp<TextStyle> => ({
+      fontSize: buttonNextLabelFontSize,
+      fontWeight: buttonNextLabelWeight,
+      color: ColorConstants.WHITE,
+    }),
+    []
+  );
+
+  const buttonNextContainerStyle = useMemo(
+    () => ({
+      height: buttonNextHeight,
+      borderRadius: StyleConstants.BORDER_RADIUS,
+      backgroundColor: ColorConstants.BLUE,
+      marginLeft: StyleConstants.MARGIN,
+      marginRight: StyleConstants.MARGIN,
+      marginBottom: StyleConstants.MARGIN,
+    }),
+    []
+  );
+
   return (
     <View>
+      <View style={titleContainerStyle}>
+        <Text style={titleTextStyle}>Login details</Text>
+      </View>
+
       <CustomTextInput
         label="EMAIL"
         onChangeTextProp={(text) => {
@@ -115,7 +210,7 @@ function SignUpFlowStepLoginDetails(props: { onNextPress: () => void }) {
       />
 
       <CustomTextInput
-        label="PASSWORD"
+        label="ENTER PASSWORD"
         onChangeTextProp={(text) => {
           setPassword(text);
         }}
@@ -131,19 +226,8 @@ function SignUpFlowStepLoginDetails(props: { onNextPress: () => void }) {
       <CustomButton
         onPress={onPress}
         labelText="Sign up"
-        labelStyle={{
-          fontSize: buttonLabelFontSize,
-          fontWeight: buttonLabelWeight,
-          color: ColorConstants.WHITE,
-        }}
-        containerStyle={{
-          height: buttonHeight,
-          borderRadius: StyleConstants.BORDER_RADIUS,
-          backgroundColor: ColorConstants.BLUE,
-          marginLeft: StyleConstants.MARGIN,
-          marginRight: StyleConstants.MARGIN,
-          marginBottom: StyleConstants.MARGIN,
-        }}
+        labelStyle={buttonNextLabelStyle}
+        containerStyle={buttonNextContainerStyle}
       />
     </View>
   );
@@ -152,38 +236,74 @@ function SignUpFlowStepLoginDetails(props: { onNextPress: () => void }) {
 export function SignUpFlow() {
   const [currentStep, setCurrentStep] = useState(0);
 
-  const onNextPress = useCallback(() => {
-    const newStep: number = currentStep + 1;
-
-    if (newStep >= SignUpFlowConfiguration.maxSteps) {
-      // TO DO: After Last Step
-      return;
-    }
-
-    setCurrentStep(newStep);
-  }, [currentStep]);
-
-  const containerStyle = useMemo(
+  const buttonPreviousContainerStyle = useMemo(
     (): StyleProp<ViewStyle> => ({
-      backgroundColor: ColorConstants.WHITE,
-      width: 390,
-      borderRadius: StyleConstants.BORDER_RADIUS,
-      justifyContent: "center",
+      height: buttonPreviousSize,
+      width: buttonPreviousSize,
+      marginTop: StyleConstants.MARGIN,
+      marginLeft: StyleConstants.MARGIN,
+      flexDirection: "row",
+      justifyContent: "flex-start",
+      alignItems: "center",
     }),
     []
   );
 
+  const arrowIconImageStyle = useMemo(
+    () => ({ height: arrowIconImageHeight, width: arrowIconImageWidth }),
+    []
+  );
+
+  const onPreviousPress = useCallback(() => {
+    const nextStep: number = currentStep - 1;
+
+    if (nextStep < SignUpFlowConfiguration.firstStep) {
+      // TO DO - Link to Login Page
+      return;
+    }
+
+    setCurrentStep(nextStep);
+  }, [currentStep]);
+
+  const onNextPress = useCallback(() => {
+    const nextStep: number = currentStep + 1;
+
+    if (nextStep >= SignUpFlowConfiguration.maxSteps) {
+      // TO DO - After Last Step
+      return;
+    }
+
+    setCurrentStep(nextStep);
+  }, [currentStep]);
+
+  const whiteContainerStyle = useMemo(
+    (): StyleProp<ViewStyle> | Animated.Animated => ({
+      backgroundColor: ColorConstants.WHITE,
+      width: whiteContainerWidth,
+      borderRadius: StyleConstants.BORDER_RADIUS,
+      justifyContent: "center",
+    }),
+    [currentStep]
+  );
+
   return (
-    <View style={containerStyle}>
-      {currentStep === 0 ? (
+    <View style={whiteContainerStyle}>
+      <Pressable onPress={onPreviousPress} style={buttonPreviousContainerStyle}>
+        <Image
+          style={arrowIconImageStyle}
+          source={require("../../assets/images/arrow_left.png")}
+        />
+      </Pressable>
+
+      {currentStep === SignUpFlowConfiguration.customerDetailsStep ? (
         <SignUpFlowStepCustomerDetails onNextPress={onNextPress} />
       ) : null}
 
-      {currentStep === 1 ? (
+      {currentStep === SignUpFlowConfiguration.loginDetailsStep ? (
         <SignUpFlowStepLoginDetails onNextPress={onNextPress} />
       ) : null}
 
-      {currentStep > 1 ? (
+      {currentStep >= SignUpFlowConfiguration.maxSteps ? (
         <View>
           <Text>Finish Sign Up</Text>
         </View>
--- frontend/src/sign-up/SignUpFlowType.ts
+++ frontend/src/sign-up/SignUpFlowType.ts
@@ -1,7 +1,13 @@
 type FlowT = {
+  firstStep: number;
   maxSteps: number;
+  customerDetailsStep: number;
+  loginDetailsStep: number;
 };
 
 export const SignUpFlowConfiguration: FlowT = {
+  firstStep: 0,
   maxSteps: 2,
+  customerDetailsStep: 0,
+  loginDetailsStep: 1,
 };
